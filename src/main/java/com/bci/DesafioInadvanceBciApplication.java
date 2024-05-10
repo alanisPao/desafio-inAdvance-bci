86e3fd87d4b444625f485776c6f10bc2d8f6047d
paola.palacios <paola.palacios@solem.cl>
2024-05-10 19:38:03 -0400

    ADD:Commit inicial, crear proyecto
    Signed-off-by: paola.palacios <paola.palacios@solem.cl>

---
+++ src/main/java/com/bci/DesafioInadvanceBciApplication.java
@@ -0,0 +1,13 @@
+package com.bci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioInadvanceBciApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioInadvanceBciApplication.class, args);
+	}
+
+}
